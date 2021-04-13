3262ea027e55bffb997bca0f4975fb96a0ae3de2
Giovanni Izzi <giovanni9972@libero.it>
2021-04-13 10:26:26 +0200

    Add card one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [GuardGuard]  }
+  { path: '', component: DashboardComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'dashboard', component: DashboardComponent, canActivate: [GuardGuard]  }
 ];
 
 @NgModule({
