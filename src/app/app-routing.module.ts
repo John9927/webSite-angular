c3311f76bcf021519a3d5b978a4c48258e1a9ea7
Giovanni Izzi <giovanni9972@libero.it>
2021-04-13 15:56:53 +0200

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [GuardGuard]  }
 ];
